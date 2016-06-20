ac8225c490ca713d212ae2cec44d6a4cd8af552a
Marcus R. Brown <me@igetgam.es>
2016-06-20 13:20:28 -0700

    Fix typo.

--- src/constants/auth.js
+++ src/constants/auth.js
@@ -1,2 +1,2 @@
 export const CLIENT_ID = '66a8b952ff7dda1027eb78b49e0d5bc8';
-export const REDIRECT_URI = `${window.location.prototcol}//${window.location.host}/callback`;
+export const REDIRECT_URI = `${window.location.protocol}//${window.location.host}/callback`;
