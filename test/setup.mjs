ca5c649cf82ac27b4778f7563234ef2f30a5289e
Marcus R. Brown <contact@marcusrbrown.com>
2022-12-24 05:43:11 -0700

    fix: use Enzyme with an adapter if the `configure` function exists

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,15 +1,26 @@
 import React from 'react';
 import { expect } from 'chai';
 import { JSDOM } from 'jsdom';
+import Enzyme from 'enzyme';
 
-const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost:8080' });
-global.window = dom.window;
-
-Object.keys(window).forEach((key) => {
-  if (!(key in global)) {
-    global[key] = window[key];
+async function setup() {
+// Temporary workaround for Enzyme 2.9.1 and React 15.6.1
+  if (typeof Enzyme.configure === 'function') {
+    const { default: Adapter } = await import('enzyme-adapter-react-15');
+    Enzyme.configure({ adapter: new Adapter() });
   }
-});
 
-global.React = React;
-global.expect = expect;
+  const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost:8080' });
+  global.window = dom.window;
+
+  Object.keys(window).forEach((key) => {
+    if (!(key in global)) {
+      global[key] = window[key];
+    }
+  });
+
+  global.React = React;
+  global.expect = expect;
+}
+
+await setup();
