fc41cf7cc4b88a36cbc6e98d2fc9ae4402e91ddf
Marcus R. Brown <contact@marcusrbrown.com>
2022-12-25 10:02:12 -0700

    fix: remove the use of Enzyme in tests (#84)

--- test/setup.mjs
+++ test/setup.mjs
@@ -1,15 +1,8 @@
 import React from 'react';
 import { expect } from 'chai';
 import { JSDOM } from 'jsdom';
-import Enzyme from 'enzyme';
 
 async function setup() {
-// Temporary workaround for Enzyme 2.9.1 and React 15.6.1
-  if (typeof Enzyme.configure === 'function') {
-    const { default: Adapter } = await import('enzyme-adapter-react-15');
-    Enzyme.configure({ adapter: new Adapter() });
-  }
-
   const dom = new JSDOM('<!doctype html><html><body></body></html>', { url: 'http://localhost:8080' });
   global.window = dom.window;
 
